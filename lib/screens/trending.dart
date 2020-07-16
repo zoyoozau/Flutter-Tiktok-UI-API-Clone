bbce487b018cfcfafdc38abd2681a969b00df424
zoyoozau <mooxaxa@gmail.com>
2020-07-16 21:06:40 +0700

    adtestFinish

--- lib/screens/trending.dart
+++ lib/screens/trending.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_admob/flutter_admob.dart';
 import 'package:getflutter/getflutter.dart';
 import 'package:tiktok/classes/tiktok.dart';
 import 'package:tiktok/config/api.dart';
@@ -21,6 +22,7 @@ class _TrendingState extends State<Trending> {
     var cookies = await api.getCookie();
     api.setCookie(cookies);
     try {
+
       var response = await http.get(
         api.url,
         headers: api.headers,
@@ -33,6 +35,8 @@ class _TrendingState extends State<Trending> {
           });
         },
       );
+      FlutterAdmob.interstital
+                    .show("ca-app-pub-3940256099942544/1033173712");
     } catch (ex) {
       print(ex);
     }
@@ -42,6 +46,7 @@ class _TrendingState extends State<Trending> {
   void initState() {
     super.initState();
     getTrending();
+    FlutterAdmob.init("ca-app-pub-6578781553594130~4859179165");
   }
 
   @override
